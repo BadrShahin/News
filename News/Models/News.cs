a464dd2921c44dd89a894664ed3f6b90c326c5da
Badr Shahin <badr.shahin2014@gmail.com>
2019-07-25 22:11:01 +0200

    Add project files.

---
+++ News/Models/News.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace News.Models
+{
+    public class News
+    {
+        [Key]
+        public long Id { get; set; }
+
+        [Required(ErrorMessage ="News Title is required")]
+        [Display(Name ="News Title")]
+        public string Title { get; set; }
+
+        [Required]
+        public int PublisherId { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime NewsDate { get; set; }
+
+        public string Content { get; set; }
+
+        public string Image { get; set; }
+
+    }
+}
---
+++ News/Models/NewsContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace News.Models
+{
+    public class NewsContext : DbContext
+    {
+        public NewsContext(DbContextOptions<NewsContext> options)
+            :base(options)
+        {   
+        }
+
+        public DbSet<News> News { get; set; }
+    }
+}
